723dc50cff667a74f7ad688f2891b9b9849d6e42
RikkiHoward <rikki.howard89@gmail.com>
2025-08-24 15:20:18 +0100

    Updated metadata.ts

--- app/bike/metadata.ts
+++ app/bike/metadata.ts
@@ -2,6 +2,6 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'The Bike – Honda CBR1000RR-R SP | Sam Cranstone',
-  description: 'Technical specifications and features of Sam Cranstone\'s 2024 Honda CBR1000RR-R Fireblade SP racing motorcycle. Interactive exploration of components and performance data.',
-  keywords: 'Honda CBR1000RR-R, Fireblade SP, motorcycle racing, BMCRC, Bemsee MRO, racing bike specifications',
+  description: 'Technical specifications and features of Sam Cranstone\'s 2024 Honda CBR1000RR-R Fireblade SP racing motorcycle tuned by mss performane. Interactive exploration of components and performance data.',
+  keywords: 'Honda CBR1000RR-R, Fireblade SP, motorcycle racing, BMCRC, Bemsee MRO, racing bike specifications, mss performance',
 };
