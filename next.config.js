569ba1ef33349374366c2619bcf1bbb1d4dd532a
RikkiHoward <rikki.howard89@gmail.com>
2025-09-07 16:11:29 +0100

    Fix Next.js project for static export on Netlify

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
   images: { unoptimized: true },
-  transpilePackages: ['framer-motion'],
-  trailingSlash: true,
-  experimental: {
-    optimizePackageImports: ['framer-motion', 'lucide-react']
-  }
 };
-
 module.exports = nextConfig;
